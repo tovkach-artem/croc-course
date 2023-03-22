f07da1d744e5d3b995eaf4cc0332bb33d015b186
TyomychTovkach <tayviscon@gmail.com>
2023-03-22 17:10:06 +0300

    Создал JavaDoc для классов пакета support: {LoggingRequired.java, ArrayOperations.java}

--- lesson2/src/main/java/ru/croc/course/lesson2/support/ArrayOperations.java
+++ lesson2/src/main/java/ru/croc/course/lesson2/support/ArrayOperations.java
@@ -2,15 +2,33 @@ package ru.croc.course.lesson2.support;
 
 import java.util.Arrays;
 
+/**
+ * Класс для вынесения общей логики по работе с массивами
+ */
 public class ArrayOperations {
+    /** Значения по умолчанию для расширения размера массива */
+    private static final int DEFAULT_MULTIPLY_VALUE = 2;
 
-    private static final int DEFAULT = 2;
-
+    /**
+     * Увеличивает размер массива заданного массива
+     * @param array исходный массив
+     * @param <T> значение обобщенного типа для массива элементов
+     * @return новый массив со скопированным элементами, но увеличивает его размерность в {@link ArrayOperations#DEFAULT_MULTIPLY_VALUE} раз
+     */
     public static <T> T[] getIncreasedSizeArray(T[] array) {
-        T[] newArray = Arrays.copyOf(array, array.length * DEFAULT);
+        T[] newArray = Arrays.copyOf(array, array.length * DEFAULT_MULTIPLY_VALUE);
         return newArray;
     }
 
+    /**
+     * Увеличивает размер массива, если это необходимо. Решение о расширении принимает на основании заполненности
+     * @param fullness заполненность массива
+     * @param array исходный массив
+     * @param <T> значение обобщенного типа для массива элементов
+     * @return либо новый массив со скопированным элементами,
+     * но увеличивает его размерность в {@link ArrayOperations#DEFAULT_MULTIPLY_VALUE} раз,
+     * либо просто старый массив
+     */
     public  static <T> T[] getFullnessSizeBasedArray(int fullness, T[] array) {
         if(fullness  ==  array.length) {
             return getIncreasedSizeArray(array);
--- lesson2/src/main/java/ru/croc/course/lesson2/support/LoggingRequired.java
+++ lesson2/src/main/java/ru/croc/course/lesson2/support/LoggingRequired.java
@@ -1,5 +1,8 @@
 package ru.croc.course.lesson2.support;
 
+/**
+ * Контракт для классов, которые требуют логирования
+ */
 public interface LoggingRequired {
     String getLoggingDescription();
 }
