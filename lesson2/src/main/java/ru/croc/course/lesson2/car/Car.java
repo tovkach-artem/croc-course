95653f6e14853d244fcd04d4b45f919daef1c3a8
TyomychTovkach <tayviscon@gmail.com>
2023-03-22 16:57:16 +0300

    Создал JavaDoc для Car.java

--- lesson2/src/main/java/ru/croc/course/lesson2/car/Car.java
+++ lesson2/src/main/java/ru/croc/course/lesson2/car/Car.java
@@ -1,21 +1,31 @@
 package ru.croc.course.lesson2.car;
 
+/**
+ * Класс машина
+ */
+
 public class Car {
+    /** номер машины*/
     private String number;
 
+    /** конструктор класса машина, позволяющий задать ей номер при создании */
     public Car(String number) {
         this.number = number;
     }
-
+    /** геттер для полян номер
+     * @return номер машины
+     */
     public String getNumber() {
         return number;
     }
-
+    /** сеттер для полян номер
+     * @param  number новое значение для поля номер
+     */
     public void setNumber(String number) {
         this.number = number;
     }
 
-
+    /** @return информация о машине в удобном для отображения виде */
     @Override
     public String toString() {
         return "Машина{" +
