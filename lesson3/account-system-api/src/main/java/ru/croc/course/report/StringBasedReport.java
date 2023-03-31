7eeba0d1e10af98018d50ed79b148582987006a4
TyomychTovkach <tayviscon@gmail.com>
2023-03-31 21:42:11 +0300

    Задокументирована работа с отчетами модуля AccountSystemApi

--- lesson3/account-system-api/src/main/java/ru/croc/course/report/StringBasedReport.java
+++ lesson3/account-system-api/src/main/java/ru/croc/course/report/StringBasedReport.java
@@ -1,5 +1,6 @@
 package ru.croc.course.report;
 
+/** Интерфейс описывает отчеты, результатом генерации которых является тип String*/
 public interface StringBasedReport extends Report<String> {
 
 }
