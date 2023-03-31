c83a03c527d8eaf33736bcac584b1886e64dbaf4
TyomychTovkach <tayviscon@gmail.com>
2023-03-31 21:50:41 +0300

    Задокументирована работа с арендой модуля AccountSystemApi

--- lesson3/account-system-api/src/main/java/ru/croc/course/rental/Rentable.java
+++ lesson3/account-system-api/src/main/java/ru/croc/course/rental/Rentable.java
@@ -1,4 +1,7 @@
 package ru.croc.course.rental;
 
+/** Интерфейс маркер, показывающий, что объект может быть арендован.
+ * Задумывалось для использования на объектах типа entity/model.
+ */
 public interface Rentable {
 }
--- lesson3/account-system-api/src/main/java/ru/croc/course/rental/RentalService.java
+++ lesson3/account-system-api/src/main/java/ru/croc/course/rental/RentalService.java
@@ -2,11 +2,15 @@ package ru.croc.course.rental;
 
 import java.time.LocalDate;
 import java.util.List;
-
+/**
+ * Интерфейс содержит общее поведение для всех сервисов аренды
+ */
 public interface RentalService<T extends Rentable> {
-
+    /** Позволяет арендовать объект (который можно арендовать -> имплементирующий интерфейс {@link Rentable}*/
     void rent(T rentable, LocalDate startDate, LocalDate endDate);
+    /** Позволяет определить арендован ли объект на определенную дату*/
     boolean isRentedOnDate(T rentable, LocalDate date);
+    /** Позволяет получить все объекты арендованные на определенную дату*/
     List<T> getRentedByDate(LocalDate date);
 
 }
