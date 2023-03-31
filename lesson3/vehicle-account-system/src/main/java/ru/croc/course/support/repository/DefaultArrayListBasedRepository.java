2351ff931b603f4ecc77b09e2753657e2131e3a9
TyomychTovkach <tayviscon@gmail.com>
2023-03-31 22:44:57 +0300

    Задокументирована работа вспомогательного пакета support модуля VehicleAccountSystem

--- lesson3/vehicle-account-system/src/main/java/ru/croc/course/support/repository/DefaultArrayListBasedRepository.java
+++ lesson3/vehicle-account-system/src/main/java/ru/croc/course/support/repository/DefaultArrayListBasedRepository.java
@@ -5,7 +5,10 @@ import ru.croc.course.support.entity.BaseEntity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+/** Дефолтная реализация интерфейса {@link Repository} для всех репозиториев, работающих с хранилищем в виде {@link ArrayList}.
+ * Данный класс позволяет избежать дублирования кода, так как реализация основных операций работы с хранилищами для разных объектов
+ * весьма схожая. Если необходимо кастомное поведение всегда можно переопределить метод в классе наследнике.
+ */
 public abstract class DefaultArrayListBasedRepository<T extends BaseEntity<ID>, ID> implements Repository<T, ID> {
     private final List<T> entities = new ArrayList<>();
 
--- lesson3/vehicle-account-system/src/main/java/ru/croc/course/support/repository/Repository.java
+++ lesson3/vehicle-account-system/src/main/java/ru/croc/course/support/repository/Repository.java
@@ -2,13 +2,18 @@ package ru.croc.course.support.repository;
 
 import ru.croc.course.support.entity.BaseEntity;
 
-import javax.management.relation.InvalidRelationIdException;
 import java.util.List;
 import java.util.Optional;
-
+/** Интерфейс содержит общее поведение для всех репозиториев */
 public interface Repository <T extends BaseEntity<ID>, ID>{
+    /** Позволяет найти объект по его уникальному идентификатору */
     Optional<T> findById(ID id);
+    /** Позволяет получить список всех объектов, которые хранятся в репозитории */
     List<T> findAll();
+    /** Позволяет сохранить объект в репозитории.
+     * Возвращает объект с установленным уникальным идентификатором.
+     */
     Optional<T> save(T entity);
+    /** Позволяет удалить объект из репозитория */
     boolean delete(T entity);
 }
