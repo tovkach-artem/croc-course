7f6efe6ad5874dc62554922831e2fc132b2f93a5
TyomychTovkach <tayviscon@gmail.com>
2023-03-31 22:04:51 +0300

    Задокументирована работа по списанию модуля AccountSystemApi

--- lesson3/account-system-api/src/main/java/ru/croc/course/decommission/DecommissionService.java
+++ lesson3/account-system-api/src/main/java/ru/croc/course/decommission/DecommissionService.java
@@ -2,9 +2,12 @@ package ru.croc.course.decommission;
 
 import java.util.List;
 
+/** Интерфейс содержит общее поведение для всех сервисов списания.
+ * Стоит отметить, что такого рода сервисы могут работать только с обслуживаемым объектами {@link  Serviceable}*/
 public interface DecommissionService<T extends Serviceable> {
-
+    /** позволяет списать объект и возвращает информацию о том, прошло ли списание успешно*/
     boolean writeOff(T serviceable);
+    /** Позволяет получить список все списанных сервисом обслуживаемых объектов */
     List<T> getAllDecommissioned();
 
 }
